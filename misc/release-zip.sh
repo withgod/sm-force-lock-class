88638b02ac48a2efd09062ae35abc40350b4f5a7
withgod <noname@withgod.jp>
2021-10-12 19:25:26 +0900

    fixed

--- misc/release-zip.sh
+++ misc/release-zip.sh
@@ -2,4 +2,4 @@
 
 cd `dirname $0`/../
 
-zip -x".gitignore" -x"README.md" -x"LICENSE" -x".git/*" -r sm-force-lock-class_0.0.0.zip .
+zip -x".gitignore" -x"misc/*" -x"README.md" -x"LICENSE" -x".git/*" -r sm-force-lock-class_0.0.0.zip .
