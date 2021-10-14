fd8082a54e6132c45320042bb8ab74115234999a
withgod <noname@withgod.jp>
2021-10-14 15:28:53 +0900

    update

--- misc/release-zip.sh
+++ misc/release-zip.sh
@@ -2,5 +2,5 @@
 
 cd `dirname $0`/../
 
-zip -x"*.zip" -x".gitignore" -x"misc/*" -x"README.md" -x"LICENSE" -x".git/*" -r sm-force-lock-class_0.0.0.zip .
+zip -x"*.zip" -x".vscode/*" -x".gitignore" -x"misc/*" -x"README.md" -x"LICENSE" -x".git/*" -r sm-force-lock-class_0.0.0.zip .
 
